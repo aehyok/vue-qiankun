18535caf0f9fe21e8d452fe1187965ceb68e4273
刘启明 <aehyok@163.com>
2021-04-18 23:09:17 +0800

    安装prettier 未生效

--- main-vite-app-ts/vite.config.ts
+++ main-vite-app-ts/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { resolve }from 'path'
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
