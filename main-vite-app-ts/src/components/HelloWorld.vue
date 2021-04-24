8f96e225a2ad7346385a73d2b560beae1197785c
刘启明 <455043818@qq.com>
2021-04-24 15:55:27 +0800

    goDb.js项目中使用，可存储数据蛮大，130000条记录没问题，性能有问题

--- main-vite-app-ts/src/components/HelloWorld.vue
+++ main-vite-app-ts/src/components/HelloWorld.vue
@@ -1,32 +1,6 @@
 <template>
   <h1>{{ msg }}</h1>
   <el-button @click="jumpClick">jumpClick</el-button>
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <script lang="ts">
