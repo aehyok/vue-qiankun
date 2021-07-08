f752d18f205824d22e00d8bcb2cb18fe07654d39
刘启明 <455043818@qq.com>
2021-07-08 17:40:35 +0800

    feat：leaflet 封装到useMap.js中

--- map-app/src/views/leaflet/marker-polygon.vue
+++ map-app/src/views/leaflet/marker-polygon.vue
@@ -41,12 +41,10 @@
   </div>
 </template>
 <script>
+import { useMap } from "./useMap";
 import { defineComponent, onMounted, reactive, ref } from "vue";
-import * as L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
-import "@geoman-io/leaflet-geoman-free";
-import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
+// import * as L from "leaflet";
+// import "leaflet/dist/leaflet.css";
 import "../../../public/L.KML.js";
 export default defineComponent({
   setup() {
@@ -56,6 +54,7 @@ export default defineComponent({
       plotPolygon: [], // Polygon多边形
     });
 
+    // const { init } = useMap();
     const mark = () => {
       map.pm.enableDraw("Marker", {});
     };
@@ -73,6 +72,7 @@ export default defineComponent({
         allowSelfIntersection: false,
       });
     };
+
     const drag = () => {
       console.log("drag");
       map.pm.enableGlobalDragMode();
@@ -102,38 +102,8 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      delete Icon.Default.prototype._getIconUrl;
-      Icon.Default.mergeOptions({
-        iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-        iconUrl: require("leaflet/dist/images/marker-icon.png"),
-        shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-      });
-
-      let array = [34.263742732916505, 108.01650524139406];
-
-      // TODO 初始化Map地图
-      map = L.map("map", {
-        zoom: 13,
-        minZoom: 10,
-        maxZoom: 22,
-        center: [array[0], array[1]],
-        doubleClickZoom: false, // 禁用双击放大
-        zoomControl: false, // 不显示默认的放大缩小按钮，可通过control.zoom进行控制
-        attributionControl: false,
-      });
-
-      // TODO 放大 缩小 按钮控制
-      L.control
-        .zoom({
-          position: "bottomleft",
-        })
-        .addTo(map);
-
-      // map.on('click', function(ev) {
-      //     console.log(ev, 'map--click')
-      // });
-
-      map.pm.setLang("zh");
+      const { init } = useMap();
+      map = init([34.263742732916505, 108.01650524139406]);
 
       // 设置绘制后的线条颜色等
       map.pm.setPathOptions({
@@ -173,31 +143,6 @@ export default defineComponent({
           console.log(event, "pm:edit拖动");
         });
       });
-
-      const satellite = L.tileLayer(
-        "https://mt{s}.sea.utuapp.cn/610403/satellite/{z}/{x}/{y}.png",
-        {
-          subdomains: ["1", "2", "3", "4"],
-        }
-      );
-      const overlay = L.tileLayer(
-        "https://mt{s}.sea.utuapp.cn/610403/overlay/{z}/{x}/{y}.png",
-        {
-          subdomains: ["1", "2", "3", "4"],
-        }
-      );
-      // 村的正射影像
-      const village = L.tileLayer(
-        "https://mt{s}.sea.utuapp.cn/610403/models/610403102210/{z}/{x}/{y}.png",
-        {
-          subdomains: ["1", "2", "3", "4"],
-          minZoom: 10,
-          maxZoom: 22, // 默认最大值位18，19后就看不到此图层
-        }
-      );
-      // 图层分组
-      const layerGroup = L.layerGroup([satellite, overlay, village]);
-      layerGroup.addTo(map);
     });
     return {
       plot,
