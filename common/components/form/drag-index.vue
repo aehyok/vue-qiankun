290816bf1dda869551bd237560a905ddb7fc91ae
刘启明 <455043818@qq.com>
2021-09-03 19:27:36 +0800

    feat: add drag选中column时的边框效果

--- common/components/form/drag-index.vue
+++ common/components/form/drag-index.vue
@@ -1,13 +1,21 @@
 <template>
   <el-row :gutter="20">
-    <draggable  v-model="columnList"   @start="drag=true" 
-  @end="drag=false"  item-key="name" class="dragClass">
-      <template #item="{ element }">
+    <draggable
+      v-model="columnList"
+      @start="dragStart"
+      @end="dragEnd"
+      item-key="name"
+      class="dragClass"
+    >
+      <template #item="{ element, index }">
         <component-view
+          @mouseover="mouseOverClick(index)"
+          @mouseleave="mouseLeaveClick(index)"
+          :class="selectIndex === index ? 'select-class' : ''"
           :columnSpan="columnSpan"
           :column="element"
           :formData="formData"
-      />
+        />
       </template>
     </draggable>
   </el-row>
@@ -38,9 +46,32 @@ const props = defineProps({
 })
 
 const drag = ref(false)
+const selectIndex = ref(-1)
+const dragStart = (event) => {
+  console.log(event, 'event')
+  drag.value = true
+}
+
+const dragEnd = (event) => {
+  console.log(event, 'dragEnd')
+}
+const mouseOverClick = (index) => {
+  selectIndex.value = index
+  console.log(index, '1111111111111')
+}
+
+const mouseLeaveClick = (index) => {
+  selectIndex.value = -1
+  console.log(index, '2222222222')
+}
 </script>
 <style lang="scss" scoped>
-.dragClass{
+.dragClass {
   width: 100%;
+}
+.select-class {
+  border: 1px dashed black;
+  cursor: pointer;
+  position: relative;
 }
 </style>
