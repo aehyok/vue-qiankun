ba17758cdff8f7ec310d9727cd1d36afcfe14711
刘启明 <455043818@qq.com>
2021-09-07 09:29:39 +0800

    chore:移除注释

--- common/services/common.js
+++ common/services/common.js
@@ -2,10 +2,6 @@ import request from "../utils/request";
 
 // 字典表访问
 export const getDictionaryList = (data) => {
-    // return request('/village/console/getBasicDictionaryList?status=1', {
-    //     method: 'get',
-    //     data,
-    // })
     return [
         {
             code: 1,
