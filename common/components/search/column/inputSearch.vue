26ca2cd3043d2669e888cd0d8d02de36e34d43e1
刘启明 <455043818@qq.com>
2021-09-30 16:05:25 +0800

    fix: search 组件拆分

---
+++ common/components/search/column/inputSearch.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- 输入框 -->
+    <el-input
+        :placeholder="item.placeholder"
+        v-model="state.keyword"
+        class="input-search"
+        @keyup.enter="search"
+    ></el-input>
+</template>
---
+++ common/components/search/column/selectSearch.vue
@@ -0,0 +1,19 @@
+<template>
+ {{ item.title }}：
+    <el-select
+          v-model="item.defaultSelected"
+          :clearable="item.showClear ? true : false"
+          class="select-search"
+          :placeholder="'请选择' + item.placeholder"
+          @change="changeSelectClick"
+          @clear="clearClick"
+        >
+          <el-option
+            v-for="item in item.dictionary"
+            :key="item.code"
+            :label="item.name"
+            :value="item.code"
+          >
+          </el-option>
+    </el-select>
+</template>
