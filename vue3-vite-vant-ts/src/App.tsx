12a0a3f7a23b2e12ef38bf14e384215d288d3ef4
刘启明 <455043818@qq.com>
2021-10-18 20:01:23 +0800

    feat: App入口改为tsx

---
+++ vue3-vite-vant-ts/src/App.tsx
@@ -0,0 +1,30 @@
+{/* <script setup lang="ts">
+</script>
+
+<template>
+<div>1111111</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> */}
+
+
+import {  defineComponent} from 'vue'
+export default defineComponent({
+  name: 'App',
+  render() {
+    return (
+      <>
+          <div>1111111111</div>
+      </>
+    )
+  }
+})
