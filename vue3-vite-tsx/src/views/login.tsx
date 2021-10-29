8c5eb4d52180cdb679a5e004abd19e264432a96e
刘启明 <455043818@qq.com>
2021-10-29 13:53:30 +0800

    feat:拆分页面到子组件

--- vue3-vite-tsx/src/views/login.tsx
+++ vue3-vite-tsx/src/views/login.tsx
@@ -2,163 +2,25 @@ import { defineComponent, ref, reactive } from 'vue'
 import { format } from 'date-fns'
 import './login.scss'
 import { TableModel } from '../types/models'
-
+import table1 from './table1'
+import table2 from './table2'
 export default defineComponent({
+  components: {
+    table1, table2
+  },
   setup() {
     const date = format(new Date(), 'yyyy-MM-dd HH:mm:ss')
-    const onChangeStatusClick =(val:any, index : number) => {
-      console.log('--index--', val, index)
-      data.test = val
-      data.inputValue = data.inputValue +'111'
-      console.log('status----',data.tableData[index].status)
-      data.tableData[index].status = !data.tableData[index].status ? 1: 0
-      data.tableData[index].times ++
-    }
-
-    
-
-    const data = reactive({
-      inputValue: '111111',
-      test: false,
-      tableData: [
-        {
-          id: 1,
-          item: '创建户码',
-          count: 1000,
-          times: 1,
-          remark: '户码默认分值',
-          status: 0,
-        },
-        {
-          id: 2,
-          item: '门牌标签',
-          count: 1000,
-          times: 0,
-          remark: '每年自动得分值，不累计叠加',
-          status: 1,
-        },
-        {
-          id: 3,
-          item: '认证我的家乡',
-          count: 1000,
-          times: 1,
-          remark: '认证成功',
-          status: 0,
-        },
-        {
-          id: 4,
-          item: '随手拍',
-          count: 5,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        },
-        {
-          id:5,
-          item: '发布农产品销售',
-          count: 5,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 1,
-        },
-        {
-          id: 6,
-          item: '查看农产品销售',
-          count: 1,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        },
-        {
-          id: 7,
-          item: '查看信息公开',
-          count: 1,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        },
-        {
-          id: 8,
-          item: '查看便民服务',
-          count: 1,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        },
-        {
-          id: 9,
-          item: '发布村友圈',
-          count: 2,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        },
-        {
-          id: 10,
-          item: '村友圈点赞',
-          count: 1,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 1,
-        },
-        {
-          id: 11,
-          item: '村友圈评价',
-          count: 1,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        },
-        {
-          id: 12,
-          item: '分享挣积分',
-          count: 2,
-          times: 0,
-          remark: '每日累计叠加',
-          status: 0,
-        }
-      ]
-    })
-    
     return () =>
       <>
         <div class="word-color">注：以家庭户为单位进行积分，户下家庭成员都可参与家庭积分。</div>
-        <el-switch v-model={data.test}  onChange ={onChangeStatusClick} />
-        <el-input v-model={data.inputValue} />
-        {date}
-        <el-table data={ data.tableData } style={{ width: '100%' }}>
-          <el-table-column prop="item" label="积分项" width="180"  align="center"/>
-          <el-table-column prop="count" label="分值" align="center">
-            {{
-              default: ({ $index, row }: { $index: number, row: any }) => {
-                return <el-input-Number controls={false} v-model={row.count}/>
-              }
-            }}
-          </el-table-column>
-          <el-table-column prop="times" label="上限次数" align="center">
-          {{
-            default: ({ row }: { $index: number,row: any }) => {
-              return (row.times > 0 ?                         
-                <el-input placeholder={"请输入---"} v-model={row.times}/> : "--")
-            }
-          }}
-          </el-table-column>
-          <el-table-column prop="remark" label="备注"  align="center" />
-          <el-table-column prop="address" label="启用状态"  align="center">
-          {{
-            default: ({ $index,row }: { $index: number,row: any }) => {
-              let value = row.status===1 ? true: false
-              return <el-switch v-model={value}  onChange ={
-                (val: any)=> {
-                  console.log(val, 'value-value')
-                  onChangeStatusClick(val,$index)
-                }
-              }/>
-            }
-          }}
-          </el-table-column>
-        </el-table>
-        <div class="word-color">每日签到（连续7天），中断后重新开始</div>
+        <table1 />
+        <div style="margin-top:45px;">每日签到（连续7天），中断后重新开始</div>
+        <table2 />
+
+        <div style="padding: 40px 0">
+          <el-button type="info">取消</el-button>
+          <el-button type="primary">确定</el-button>
+        </div>
       </>
   }
 })
