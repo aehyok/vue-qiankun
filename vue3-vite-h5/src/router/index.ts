5dc8bc01d157e3d657a13bf41fc7c16406ad7759
刘启明 <455043818@qq.com>
2021-10-29 15:51:32 +0800

    feat: 路由引入，以及home页面添加

---
+++ vue3-vite-h5/src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+    meta: {
+      title: "首页",
+      showLayout: false,
+    }
+  }
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+// router.beforeEach((to, _from, next) => {
+//   next()
+// })
+
+// // 页面进入之后
+// router.afterEach(() => {
+// })
+
+export default router
---
+++ vue3-vite-h5/src/views/Home.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { Button as VanButton } from 'vant';
+console.log(['11home'])
+const onClick = () => {
+  alert(1111)
+}
+</script>
+
+<template>
+  <div class="word-color">11111111</div>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <van-button type="primary" @click="onClick">主要按钮</van-button>
+</template>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
