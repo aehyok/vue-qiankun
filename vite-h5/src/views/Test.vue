321551d8e06ba348a9da965831e68204bc7ee3a5
刘启明 <455043818@qq.com>
2021-10-31 16:40:55 +0800

    feat: 引入editorconfig 配置工具

--- vite-h5/src/views/Test.vue
+++ vite-h5/src/views/Test.vue
@@ -1,39 +1,36 @@
 <template>
-    <van-nav-bar title="积分明细" left-arrow fixed />
-    <div class="container">
-        <template v-for="(item, index) in state.list" :key="item" >
-            <van-sticky :offset-top="50" v-if="item % 10 === 1" >
-                <div style="background: #07c160;width: 100px;">
-                    {{ item }}
-                </div>
-            </van-sticky>
-            <div class="item item-1" v-else-if="index ===1">{{item}}</div>
-            <div class="item" v-else>{{item}}</div>
-        </template>
-    </div>
+  <van-nav-bar title="积分明细" left-arrow fixed />
+  <div class="container">
+    <template v-for="(item, index) in state.list " :key="item">
+      <van-sticky :offset-top="50" v-if="item % 10 === 1">
+        <div style="background: #07c160;width: 100px;">{{ item }}</div>
+      </van-sticky>
+      <div class="item item-1" v-else-if="index === 1">{{ item }}</div>
+      <div class="item" v-else>{{ item }}</div>
+    </template>
+  </div>
 </template>
- 
+
 <script lang="ts" setup>
-import {  reactive,ref } from 'vue'
+import { reactive, ref } from 'vue'
 import {
-    NavBar as VanNavBar,
-    Sticky as VanSticky,
+  NavBar as VanNavBar,
+  Sticky as VanSticky,
 } from 'vant'
 let state = reactive({
-    list: []
+  list: []
 })
 
 for (let i = 0; i < 100; i++) {
-    state.list.push(state.list.length + 1 )
+  state.list.push(state.list.length + 1)
 }
 </script>
 
 <style lang="scss" scoped>
 .item {
-    border:1px solid red;
+  border: 1px solid red;
 }
 .item-1 {
-    margin-top: 50px;
+  margin-top: 50px;
 }
-
 </style>
--- vite-h5/src/views/main/home.vue
+++ vite-h5/src/views/main/home.vue
@@ -1,7 +1,7 @@
 <template>
-    <div style="height: 200px;border: 1px solid red;">Home</div>
-    <div style="height: 200px;border: 1px solid red;">Home</div>
+  <div style="height: 200px;border: 1px solid red;">Home</div>
+  <div style="height: 200px;border: 1px solid red;">Home</div>
 </template>
 <script lang="ts" setup>
-    console.log('home-index')
+console.log('home-index')
 </script>
