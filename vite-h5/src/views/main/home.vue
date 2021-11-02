0990749a4a1f353e64b2a015497428f259dd3f2e
刘启明 <455043818@qq.com>
2021-11-02 18:34:39 +0800

    css: style scoped module 样式范例，三种格式

--- vite-h5/src/views/main/home.vue
+++ vite-h5/src/views/main/home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div style="height: 200px; border: 1px solid red">Home</div>
+  <div :class="classes.homered">Home</div>
   <van-button type="primary" @click="jump">测试跳转</van-button>
-  <div style="height: 200px; border: 1px solid red">Home</div>
+  <div :class="$style.hometwo">Home</div>
+  <div class="homethree">Home</div>
 </template>
 <script lang="ts" setup>
   import { Button as VanButton } from 'vant';
@@ -13,4 +14,32 @@
     console.log('jump--jump');
     router.push('/test');
   };
+
+  const theme = {
+    height: '200px',
+    border: '1px solid green',
+  };
 </script>
+
+<style module="classes">
+  .red {
+    color: red;
+  }
+  .homered {
+    height: 200px;
+    border: 1px solid red;
+  }
+</style>
+
+<style module>
+  .hometwo {
+    height: 200px;
+    border: 1px solid blue;
+  }
+</style>
+<style lang="scss" scoped>
+  .homethree {
+    height: v-bind('theme.height');
+    border: v-bind('theme.border');
+  }
+</style>
