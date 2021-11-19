e83d4ea7c9e5138ba20434ae70925394c590a649
刘启明 <455043818@qq.com>
2021-11-19 20:58:17 +0800

    feat: 实现罗马数字转整数

--- vite-h5/src/views/Test.tsx
+++ vite-h5/src/views/Test.tsx
@@ -2,6 +2,86 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup() {
     console.log('table in console')
+    function convertTest(input: string): number {
+
+      let obj = new Map()
+      obj.set('I', 1)
+      obj.set('V', 5)
+      obj.set('X', 10)
+      obj.set('L', 50)
+      obj.set('C', 100)
+      obj.set('D', 500)
+      obj.set('M', 1000)
+
+
+      let spec = new Map()
+      spec.set('CM', 900)
+      spec.set('CD', 400)
+      spec.set('XC', 90)
+      spec.set('XL', 40)
+      spec.set('IX', 9)
+      spec.set('IV', 4)
+
+      let output = 0
+      for (let item of spec.keys()) {
+        console.log(input,item, 'spec----------------------')
+        if(input.indexOf(item)> -1 ) {
+          console.log(item,spec[item], 'up')
+          input = input.replace(item, '');
+          output = output+ spec.get(item)
+          console.log(spec,input, 'input-spec',output)
+        }
+      }
+
+
+      console.log('input=--中间', input,output)
+      let array = input.split('')
+      array.forEach((item) => {
+        console.log(item, 'down')
+        if(obj.has(item)) {
+          input = input.replace(item, '');
+          output = output + obj.get(item)
+        }
+        console.log(input, 'input-obj',output)
+      });
+      console.log(output, 'output-ending')
+      return output;
+    }
+
+    // convertTest('III');
+    // convertTest('IV');
+    // convertTest('LVIII');
+    // convertTest('MCMXCIV');
+
+
+
+    const romanToInt = function(s) {
+      const map = {
+          I: 1,
+          V: 5,
+          X: 10,
+          L: 50,
+          C: 100,
+          D: 500,
+          M: 1000
+      }
+      let sum = 0;
+      for(let i = 0; i < s.length; i++) {
+          const item = s[i];
+          let val = map[item];
+          console.log(val,map[s[i+1]], '比较' )
+          if(val < map[s[i+1]]) {
+              // 前面小于后面 后面 - 前面
+              val = map[s[i+1]] - val;
+              i++;
+          }
+          sum += val;
+      }
+      console.log(sum, 'sum')
+      return sum
+  };
+
+  romanToInt('IV')
     return () =>
       <>
         Test
