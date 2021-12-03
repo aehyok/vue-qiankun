c1ebf4857a8ff1cc3706817569ef3a11d8668e16
刘启明 <455043818@qq.com>
2021-12-03 20:02:35 +0800

    chore: 移除taro 小程序项目文件到单独的项目

--- taro-vue3-nutui/src/components/compatible.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  // 是否支持自定义头部
-  isCustomHeader: process.env.TARO_ENV !== 'alipay',
-};
--- taro-vue3-nutui/src/utils/useRequest.ts
+++
@@ -1,5 +0,0 @@
-import Taro from '@tarojs/taro';
-
-export function useRequest() {
-    
-} 
