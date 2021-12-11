0c4ff6771f76121713d78fbd75bd28dd839c7111
刘启明 <455043818@qq.com>
2021-12-11 15:58:37 +0800

    feat： react umi路由跳转，并引入antd UI

--- react-app/.umirc.ts
+++ react-app/.umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   },
   routes: [
     { path: '/', component: '@/pages/index' },
+    { path: '/home', component: '@/pages/home' },
   ],
   fastRefresh: {},
 });
--- react-app/src/pages/index.tsx
+++ react-app/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import styles from './index.less';
+import { Link, history } from 'umi';
+import { Button} from 'antd';
+
 
 export default function IndexPage() {
+  const routerClick = () => {
+    history.push('/home')
+  }
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Link to="/home">Go to list page</Link>
+      <Button onClick={routerClick}>Go to list page</Button>
     </div>
   );
 }
