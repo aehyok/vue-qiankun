439d01aca727060836fd757416a30c2e9c946faa
刘启明 <455043818@qq.com>
2021-12-24 16:45:55 +0800

    feat: react table+ useState绑定

--- react-app/src/pages/home/index.tsx
+++ react-app/src/pages/home/index.tsx
@@ -1,9 +1,43 @@
 import styles from './index.less';
-
+import { Table } from 'antd';
 export default function IndexPage() {
+  const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
+
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
   return (
     <div>
       <h1 className={styles.title}>Page Home</h1>
+      <Table dataSource={dataSource} columns={columns} />;
     </div>
   );
 }
--- react-app/src/pages/index.tsx
+++ react-app/src/pages/index.tsx
@@ -1,17 +1,25 @@
 import styles from './index.less';
 import { Link, history } from 'umi';
 import { Button} from 'antd';
+import { useState } from 'react'
 
 
 export default function IndexPage() {
   const routerClick = () => {
     history.push('/home')
   }
+  const [on, setOn] = useState(false);
+
+  const lightOn = () => setOn(true);
+  const lightOff = () => setOn(false);
   return (
     <div>
+      <div className={on ? styles.bulbon : styles.bulboff} />
+      <button onClick={lightOn}>开灯</button>
+      <button onClick={lightOff}>关灯</button>
       <h1 className={styles.title}>Page index</h1>
       <Link to="/home">Go to list page</Link>
-      <Button onClick={routerClick}>Go to list page</Button>
+      <Button  type="primary" onClick={routerClick}>Go to list page</Button>
     </div>
   );
 }
