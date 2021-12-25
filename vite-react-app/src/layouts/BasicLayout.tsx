2474a625e7f87ada86b6dcbaec8fbc369d4a6020
aehyok <455043818@qq.com>
2021-12-26 07:14:38 +0800

    feat:react 首页模版布局优化调整

--- vite-react-app/src/layouts/BasicLayout.tsx
+++ vite-react-app/src/layouts/BasicLayout.tsx
@@ -9,9 +9,29 @@ const { Content } = Layout;
 const BasicLayout: React.FC<{ route: IRouteConfig }> = ({ route }) => {
   return (
       <>
-    <div>user上半部分</div>
-    {renderRoutes(route.routes)}
-    <div>下半部分user</div>
+         <Layout style={{ height: '100vh' }}>
+         <MyLayout />
+         <Layout>
+           <MyLeftSide />
+           <Layout style={{ padding: '0 24px 24px' }}>
+             <Breadcrumb style={{ margin: '16px 0' }}>
+               <Breadcrumb.Item>Home</Breadcrumb.Item>
+               <Breadcrumb.Item>List</Breadcrumb.Item>
+               <Breadcrumb.Item>App</Breadcrumb.Item>
+             </Breadcrumb>
+             <Content
+               className="site-layout-background"
+               style={{
+                 padding: 24,
+                 margin: 0,
+                 minHeight: 280,
+               }}
+             >
+                {renderRoutes(route.routes)}
+             </Content>
+           </Layout>
+         </Layout>
+       </Layout >
     </>
   )
 }
@@ -21,26 +41,3 @@ export default BasicLayout
 
 
 
-//   <Layout style={{ height: '100vh' }}>
-//         <MyLayout />
-//         <Layout>
-//           <MyLeftSide />
-//           <Layout style={{ padding: '0 24px 24px' }}>
-//             <Breadcrumb style={{ margin: '16px 0' }}>
-//               <Breadcrumb.Item>Home</Breadcrumb.Item>
-//               <Breadcrumb.Item>List</Breadcrumb.Item>
-//               <Breadcrumb.Item>App</Breadcrumb.Item>
-//             </Breadcrumb>
-//             <Content
-//               className="site-layout-background"
-//               style={{
-//                 padding: 24,
-//                 margin: 0,
-//                 minHeight: 280,
-//               }}
-//             >
-//                {renderRoutes(route.routes)}
-//             </Content>
-//           </Layout>
-//         </Layout>
-//       </Layout >
