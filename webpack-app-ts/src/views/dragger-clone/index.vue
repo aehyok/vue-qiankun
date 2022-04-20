c2764782dc09a89140ac2c831bacee284b0a5c59
刘启明 <455043818@qq.com>
2022-04-20 20:38:46 +0800

    Update index.vue

--- webpack-app-ts/src/views/dragger-clone/index.vue
+++ webpack-app-ts/src/views/dragger-clone/index.vue
@@ -1,11 +1,101 @@
 <template>
-        3333333333333
+  <div class="row">
+    <div class="col-3">
+      <h3>Draggable 1</h3>
+      <draggable
+        class="dragArea list-group"
+        :list="list1"
+        :group="{ name: 'people', pull: 'clone', put: false }"
+        @change="log"
+        item-key="name"
+      >
+        <template #item="{ element }">
+          <div class="list-group-item">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+    </div>
+
+    <div class="col-3">
+      <h3>Draggable 2</h3>
+      <draggable
+        class="dragArea list-group"
+        :list="list2"
+        group="people"
+        @change="log"
+        item-key="name"
+      >
+        <template #item="{ element }">
+          <div class="list-group-item">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+    </div>
+
+    <div class="col-3">
+      <h3>Draggable 3</h3>
+      <draggable
+        class="dragArea list-group"
+        :list="list2"
+        group="people"
+        @change="log"
+        item-key="name"
+      >
+        <template #item="{ element }">
+          <div class="list-group-item">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+    </div>
+
+    <!-- <rawDisplayer class="col-3" :value="list1" title="List 1" />
+
+    <rawDisplayer class="col-3" :value="list2" title="List 2" /> -->
+  </div>
 </template>
-    
-<script setup lang='ts'>
-    
+
+<script>
+import draggable from "vuedraggable";
+export default {
+  name: "clone",
+  display: "Clone",
+  order: 2,
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      list1: [
+        { name: "John", id: 1 },
+        { name: "Joao", id: 2 },
+        { name: "Jean", id: 3 },
+        { name: "Gerard", id: 4 },
+      ],
+      list2: [
+        { name: "Juan", id: 5 },
+        { name: "Edgard", id: 6 },
+        { name: "Johnson", id: 7 },
+      ],
+      list3: [
+        { name: "Juan-1", id: 15 },
+        { name: "Edgard-2", id: 16 },
+        { name: "Johnson-3", id: 17 },
+      ],
+    };
+  },
+  methods: {
+    log: function (evt) {
+      window.console.log(evt);
+    },
+  },
+};
 </script>
-    
-<style>
-    
+<style scoped lang="scss">
+.row {
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+}
 </style>
