eff7b2ca522828b10ed039a519efdfb6cc48d6d8
aehyok <455043818@qq.com>
2022-04-30 21:48:17 +0800

    feat: Suspense

---
+++ vite-vue+react+demo/vite-h5/src/views/suspense/index.vue
@@ -0,0 +1,17 @@
+<template>
+<Suspense>
+  <!-- 具有深层异步依赖的组件 -->
+  <Dashboard />
+
+  <!-- 在 #fallback 插槽中显示 “正在加载中” -->
+  <template #fallback>
+    Loading...
+  </template>
+</Suspense>
+</template>
+
+<script lang="ts" setup>
+// https://juejin.cn/post/6854573214547312654
+// https://juejin.cn/post/7055098445467484167
+import { Suspense } from 'vue'
+</script>
