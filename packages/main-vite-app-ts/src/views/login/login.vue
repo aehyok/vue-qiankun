ecc6e95c3e62a4b4cab2a2796baa18799d314a8b
刘启明 <455043818@qq.com>
2022-05-18 14:22:30 +0800

    feat: 登录组件样式布局调整

--- packages/main-vite-app-ts/src/views/login/login.vue
+++ packages/main-vite-app-ts/src/views/login/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-page">
     <div class="left-login"></div>
-    <div class="right-login"></div>
+    <!-- <div class="right-login"></div> -->
     <div class="content-wrapper">
       <div class="project-name">{{ projectName }}</div>
       <el-form
@@ -12,29 +12,55 @@
         @submit.native.prevent
       >
         <el-form-item prop="account">
-          <img src="../../../public/images/login/icon-1.png" />
-          <el-input type="text" placeholder="用户名" v-model="loginForm.account" />
+          <el-input
+            type="text"
+            placeholder="登录账号"
+            v-model="loginForm.account"
+            width="100%"
+          >
+           <template #prefix>
+              <img class="project-img" :src="loginForm.account.length ==0?'/images/login/icon-one.png':'/images/login/icon-onel.png'"/>
+           </template>
+          </el-input>
         </el-form-item>
+
         <el-form-item prop="password">
-          <img src="../../../public/images/login/icon-2.png" />
-          <el-input type="password" placeholder="密码" v-model="loginForm.password" />
+          <el-input
+            type="password"
+            placeholder="登录密码"
+            v-model="loginForm.password"
+          >
+           <template #prefix>
+               <img class="project-img" :src="loginForm.password.length ==0?'/images/login/icon-two.png':'/images/login/icon-twol.png'"/>
+           </template>
+          </el-input>
         </el-form-item>
         <el-form-item class="smbot" prop="captchaValue">
           <div class="form-row">
-            <img src="../../../public/images/login/icon-2.png" />
-            <el-input placeholder="验证码" v-model="loginForm.captchaValue" style="flex: 1" />
-            <div class="verCodeImg">
+            <el-input
+              placeholder="验证码"
+              v-model="loginForm.captchaValue"
+              style="flex: 1"
+            />
+           
+           <div class="verCodeImg">
               <img :src="loginForm.vcodeImg" />
-              <!-- <div class="svgbox" v-html="vcodeImg"></div> -->
-              <span @click="getImageVerifyCode" style="margin-left: 20px; cursor: pointer">
+              <span
+                @click="getImageVerifyCode"
+                style="margin-left: 20px; cursor: pointer"
+              >
                 换一张
               </span>
             </div>
           </div>
         </el-form-item>
         <div class="form-row">
-          <el-checkbox v-model="rememberPasCbox" size="large"> 记住用户名和密码 </el-checkbox>
-          <el-button type="default"  text @click="dialogVisible = true" style="color: #0091ff"> 忘记密码 </el-button>
+          <el-checkbox v-model="rememberPasCbox" size="large">
+            记住用户名和密码
+          </el-checkbox>
+          <el-button  @click="dialogVisible = true">
+            忘记密码?
+          </el-button>
         </div>
         <el-button
           size="large"
@@ -159,93 +185,148 @@
 </script>
 
 <style lang="scss" scoped>
-  .login-page {
-    width: 100%;
+.login-page {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  background: url("../../../public/images/login/bg.png");
+  background-size: 100% 100%;
+  .left-login {
+    width: 50vw;
+    height: 82vh;
+    max-height: 1081px;
+    background: url("../../../public/images/login/bg_nav.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    margin-top: 40px;
+    margin-left: 39px;
+  }
+  .right-login {
+    width: 426px;
     height: 100vh;
+    max-height: 1081px;
+    background: url("../../../public/images/login/bg_right.png");
+    background-size: 100% 100%;
+  }
+  .smbot{
+    border: none;
+  }
+}
+.content-wrapper {
+  position: absolute;
+  top: 50%;
+  right: 12vw;
+  transform: translate(0, -50%);
+  // background: url("../../../public/images/login/bg_login.png");
+  background-size: 100% 100%;
+  // width: 561px;
+  // height: 898px;
+  // padding-top: 180px;
+  width: 390px;
+  height: 413px;
+  background: #FFFFFF;
+  border-radius: 12px;
+  opacity: 0.95;
+  box-sizing: border-box;
+  .project-name {
+    text-align: center;
+    font-size: 24px;
+    margin-top: 40px;
+    margin-bottom: 30px;
+    font-weight: 600;
+    color: #1C63F0;
+  }
+  .project-img{
+    width: 20px;
+    height: 20px;
+    vertical-align: middle;
+  }
+  .verCodeImg {
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    .svgbox {
+      position: relative;
+      top: 6px;
+    }
+    img {
+      height: 100%;
+    }
+  }
+  .form-class {
+    // padding: 72px 90px;
+  }
+  :deep(.el-input__inner) {
+    height: 40px;
+    border: none;
+  }
+
+
+  .smbot :deep(.el-input__inner) {
+    height: 40px;
+    // border-radius: 2px;
+    // border: 1px solid #9EA7B4;
+  }
+
+
+  :deep(.el-form-item__content) {
+    width: 350px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+    border: 1px solid rgba(158, 167, 180, 1);
+    background: rgba(243, 246, 249, 0.2);
+    // :focus{
+    //   border: 1px solid #1C63F0;
+    // background: rgba(243, 246, 249, 0.2);
+    // }
+  }
+  .smbot :deep(.el-form-item__content) {
+    border: none;
+    // :focus{
+    //   border: 1px solid #1C63F0;
+    // }
+  }
+  .form-row {
     display: flex;
     justify-content: space-between;
-    background: url("../../../public/images/login/bg.png");
-    background-size: 100% 100%;
-    .left-login {
-      width: 1110px;
-      height: 100vh;
-      max-height: 1081px;
-      background: url("../../../public/images/login/bg_left.png");
-      background-size: 100% 100%;
-    }
-    .right-login {
-      width: 426px;
-      height: 100vh;
-      max-height: 1081px;
-      background: url("../../../public/images/login/bg_right.png");
-      background-size: 100% 100%;
-    }
+    align-items: center;
+    font-size: 14px;
+    font-weight: 400;
+    color: #657180;
   }
-  .content-wrapper {
-    position: absolute;
-    top: 50%;
-    right: 120px;
-    transform: translate(0, -50%);
-    background: url("../../../public/images/login/bg_login.png");
-    background-size: 100% 100%;
-    width: 561px;
-    height: 898px;
-    padding-top: 180px;
-    box-sizing: border-box;
-    .project-name {
-      text-align: center;
-      font-size: 24px;
-      font-weight: 500;
-      color: #0091ff;
-      line-height: 33px;
-    }
-    .verCodeImg {
-      margin-left: 10px;
-      display: flex;
-      align-items: center;
-      .svgbox {
-        position: relative;
-        top: 6px;
-      }
-      img {
-        height: 100%;
-      }
-    }
-    .form-class {
-      padding: 72px 90px;
-    }
-    :deep(.el-input__inner) {
-      height: 50px;
-      border: none;
-      background: none;
-      &::placeholder {
-        color: #0091ff;
-      }
-    }
-    :deep(.el-form-item__content) {
-      display: flex;
-      align-items: center;
-      border-bottom: 1px solid #0091ff;
-    }
-    .form-row {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      font-size: 14px;
-      color: #0091ff;
-    }
-    .el-checkbox {
-      color: #0091ff;
-    }
-    .btn-class {
-      width: 100%;
-      margin-top: 20px;
-      height: 50px;
-      background: linear-gradient(180deg, #00b3ff 0%, #007cff 100%);
-    }
-    :deep(input:-webkit-autofill) {
-      box-shadow: 0 0 1000px 0 #071b5a inset !important;
-      -webkit-text-fill-color: #fff;
-    }
+  .el-checkbox {
+    font-size: 14px;
+    font-weight: 400;
+    color: #657180;
   }
+  .el-button{
+    font-size: 14px;
+    font-weight: 400;
+    color: #657180;
+  }
+  .btn-class {
+    width: 100%;
+    margin-top: 10px;
+    height: 40px;
+    background: #1C63F0;
+    border-radius: 2px;
+    font-size: 16px;
+    font-weight: 400;
+    color: #FFFFFF;
+    margin-bottom: 50px;
+  }
+  :deep(input:-webkit-autofill) {
+  //   box-shadow: 0 0 1000px 0 #071b5a inset !important;
+  //   -webkit-text-fill-color: #fff;
+  }
+  :deep(.el-form-item){
+    margin-bottom: 15px !important;
+  }
+  :deep(.el-form.form-class){
+    padding: 0 20px;
+  }
+}
 </style>
